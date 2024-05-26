99f53795ca17dad91c6b4e76ada33965b7c70b30
KDCG <kd_elite@sjtu.edu.cn>
2024-05-26 14:20:38 +0800

    Using opencv, adjust Cmake

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,5 @@
 #include "tree.h"
-#include "picture_process.h"
+#include <opencv2/opencv.hpp>
 #define LEARNING_RATE 0.01
 #define ITERATIONS 1000
 
@@ -16,19 +16,29 @@ void initial_vertex() {
 }
 
 int main() {
-    PictureProcess pictureProcess;
-    pictureProcess.read_image("../input/input.pgm");
+    cv::Mat image = cv::imread("../input/input.pgm", cv::IMREAD_GRAYSCALE);
+    image.convertTo(image, CV_64F, 1.0 / 255.0);
+    if (image.empty()) {
+        std::cout << "Failed to read image" << std::endl;
+        return -1;
+    }
 
-    height = pictureProcess.height;
-    width = pictureProcess.width;
+    width = image.cols;
+    height = image.rows;
+
 
     Tree tree(0, height, 0, width);
     initial_vertex();
-
+    
+    printf("Start training\n");
     for (int i = 0; i < ITERATIONS; i++) {
+        if (i % 100 == 0)
+            printf("Iteration %d\n", i);
+        
+
         for (int x = 0; x < height; x++) {
             for (int y = 0; y < width; y++) {
-                tree.grad_backwards(x, y, pictureProcess.map[x][y]);
+                tree.grad_backwards(x, y, image.at<double>(x, y));
             }
         }
         tree.grad_update(LEARNING_RATE);
@@ -38,11 +48,13 @@ int main() {
     for (int i = 0; i < height; i++) {
         for (int j = 0; j < width; j++) {
             double col = tree.get_col(i, j);
-            pictureProcess.map[i][j] = col;
+            image.at<double>(i,j) = col;
         }
     }
 
-    pictureProcess.write_image("../output/output.ppm");
+    cv::Mat output_image;
+    image.convertTo(output_image, CV_8U, 255.0);
+    cv::imwrite("../output/output_1.pgm", output_image);
 
     return 0;
 }
